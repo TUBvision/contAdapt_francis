c9fd14836e09de62139feb26ca7a151fe7f7eb5e
Will Baker Morrison <willbakermorrison@gmail.com>
2016-09-22 15:04:32 +0200

    update beats

--- Code/BEATS.py
+++ Code/BEATS.py
@@ -2,66 +2,92 @@
 """
 Created on Thu Mar  3 11:33:57 2016
 
-@author: will
-"""
-import numpy as np
-import sympy as sp
-import scipy.ndimage.filters as flt
-#import square_wave
-import matplotlib.pyplot as plt
-#import cv2
-from PIL import Image
-from multiprocessing import Pool
-
-# BEATS FILLING IN
-"""
-Task of visual system to create normalized image
-- Global search for min/max of inputs simulating cellular adaptation
+GENERAL OVERVIEW 
+------------------------------------------------------------------------------
+Relevance overview
+------------------------------------------------------------------------------
+- One task of visual system to create normalized image
+- This involves global search for min/max of inputs 
 - Inputs are rescaled based on these values
-- This exact method of normalization is not NPL plausible
+- This exact method of normalization is not nuero-phys. plausible
+- However, it could serve as inspiration for diffusion-like filling-in process
+  to make the FACADE-like model by Gregory Francis more neuro-phys. plausible, 
+  as well as, producing a more accurate output percept.
 
+------------------------------------------------------------------------------
+Meta-description of model
+------------------------------------------------------------------------------
 - Here exact normalization achieved by next-neighbour interactions
-- Uses novel non-linear diffusuion systems (like filling-in)
-- Contrast borders are flooded with activity
+- Uses novel non-linear diffusuion systems (like filling-in) this is where
+  Bigger EAts Smaller (BEATS), and regions of maximum brightness/darkness grow
+  until a global min/max is reached.
 - Reaction-diffusion system, Laplacian diffusion model a continuum of electrically
   couple cells (syncytium)
 - Laplacian describes an electrical synapse (gap-junction) permitting
   hyperpolarizing and depolarizing somatic curents.
   
+  Based on a smodified reaction-diffusion system (Laplacian diffusion)
   
-  
-  Modified reaction-diffusion system (Laplacian diffusion)
+------------------------------------------------------------------------------
+Notes
+------------------------------------------------------------------------------
+- Majority of the code is dedicated to solving various ordinary differential 
+  equations (ODE's), which model various synaptic interactions.
+- The solving method chosen here is the Fourth-Order Runga-Kutta (RK4) method. 
+  One of the most robust and accurate method for solving ODE's. Keil tends to 
+  use the Euler method to solve it, as it's simpler but less accurate. 
 """
 
-
+import numpy as np
+import sympy as sp
+import scipy.ndimage.filters as flt
+#import square_wave
+import matplotlib.pyplot as plt
+from PIL import Image
+from multiprocessing import Pool
     
 def T(lamda,x): 
     """
-    T Operator
-    lambda is a "steering" constant between 3 behaviour states
-    -----------------------------
-    0     -> linearity 
-    +inf  -> max
-    -inf  -> min
-    -----------------------------
+    T Operator function
+    ---------------------------------------------------------------------------
+    lambda is a "steering" constant between 3 diffusion behaviour states, as a
+    part of the diffusion operator. The consequences of steering being:
+    
+    -------------------------------------------------------
+    |lamba |   state   | result on input x                |
+    |------|-----------|----------------------------------|
+    |==0   | linearity | Remains the same                 |
+    |<0    | max       | Half-wave rectification          |
+    |>0    | min       | Inverse half-wave rectification  |
+    -------------------------------------------------------
+    
+    Parameters
+    ------------    
+    lambda : int    steering constant
+    x :array-like   input stimulus arrau    
+    
     """    
-    if lamda == 0:  # linearity
+    if lamda == 0:  
         return x
-    elif lamda > 0: #  Half-wave rectification
+    elif lamda > 0:
         maxval = np.zeros_like(x)
         return np.array([x, maxval]).max(axis=0)
-    elif lamda < 0: # Inverse half-wave rectification
+    elif lamda < 0: 
         minval = np.zeros_like(x)
         return np.array([x, minval]).min(axis=0)
         
 
-def Diffusion_operator(lamda,f,t): # 2D Spatially Discrete Non-Linear Diffusion
+def Diffusion_operator(lamda,f,t):  
     """
-    Diffusion Operator
-    ------------------------------------------
-    Special case where lambda == 0, operator becomes Laplacian        
-    
-    
+    Diffusion Operator - 2D Spatially Discrete Non-Linear Diffusion
+    ---------------------------------------------------------------------------
+    This describes the synaptic states of diffusion processes and correspond to 
+    It "models different types of electrical synapses (gap junctions).
+    The linear version, Kλ=0 , describes the exchange of both polarizing and 
+    de-polarizing". Thus the rectification found in the T operator serves to 
+    dictate whether polizing (half-wave rectification) or de-polizing (inverse
+    half-wave rectification) flow states are allowed to occur.
+
     Parameters
     ----------
     D : int                      diffusion coefficient
@@ -72,15 +98,21 @@ def Diffusion_operator(lamda,f,t): # 2D Spatially Discrete Non-Linear Diffusion
     Returns
     ----------
     f : array-like               output of diffusion equation
-    -----------------------------
-    0     -> linearity (T[0])
-    +inf  -> positive K(lamda)
-    -inf  -> negative K(lamda)
-    -----------------------------
+    
+    
+    ---------------------------------------------------------------------
+    |lamba |   state                 | result on input x                |
+    |------|-------------------------|----------------------------------|
+    |==0   | linearity  (T[0])       | Operator becomes Laplacian       |
+    |<0    | positive K(lamda)       | Half-wave rectification          |
+    |>0    | negative K(lamda)       | Inverse half-wave rectification  |
+    ---------------------------------------------------------------------
+    
     """
+    
     if lamda == 0:  # linearity
         return flt.laplace(f)
-    else:           # non-linearity
+    else:           # non-linearity (neighbour interactions up/down/left/right)
         f_new = T(lamda,np.roll(f,1, axis=0)-f) \
         +T(lamda,np.roll(f,-1, axis=0)-f) \
         +T(lamda,np.roll(f, 1, axis=1)-f) \
@@ -89,28 +121,36 @@ def Diffusion_operator(lamda,f,t): # 2D Spatially Discrete Non-Linear Diffusion
 
 
 def Dirac_delta_test(tester):
-    # Limit injection to unitary multiplication, not infinite
+    """
+    The stimuli is injected at t=0 with a dirac delta function. This function
+    limits the injection to a unitary multiplication, rather than infinite.
+    """
     if np.sum(sp.DiracDelta(tester)) == 0:
         return 0
     else:
         return 1
 
-def Runge_Kutta(stimulus,lamda,t0,h,N,D,t_N):
+def Solve_diff_eq_RK4(stimulus,lamda,t0,h,N,D,t_N):
     """
-    4th order Runge-Kutta solution to:
-    linear and spatially discrete diffusion equation (ignoring leakage currents)
+    Finds a solution to the linear and spatially discrete diffusion equation:
     
-    Adiabatic boundary conditions prevent flux exchange over domain boundaries
-        
+                        df/dt = D*K(f) - s*delta(t-t0)  
+                        
+    using the 4th order Runge-Kutta method (ignoring leakage currents). The 
+    stimulus is inject at t0 and then the defined lamda value permits certain
+    diffusion behaviour to occur iteratively over this initial stimuli. The
+    result is a diffusion of the maximum and minimum values across the stimuli
+    to obtain a global value of luminance extremities through local interactions.   
     
     Parameters
     ---------------
-    stimulus : array_like   input stimuli [t,x,y]
-    lamda : int             0 +/- inf
+    stimulus : array_like   input stimuli [x,y] "photo receptor activity elicited by
+                            some real-world luminance distribution"
+    lamda : int             a value between +/- inf
     t0 : int                point of stimulus "injection"
-    h : int                 step size
-    N : int                 array size (stimulus.shape[1])
-    D : int                 diffusion coefficient [constant]
+    h : int                 Runga-kutta step size
+    N : int                 stimulus array size
+    D : int                 diffusion coefficient weights rate of diffusion [0<D<1]
     
     Returns
     ----------------
@@ -151,15 +191,20 @@ def Runge_Kutta(stimulus,lamda,t0,h,N,D,t_N):
         return f
 
 
-def ONtype_norm(s,t0,h,N,D,t_N,a,b,R,dt=0.001):
+def ONtype_norm(s,t0,h,N,D,t_N,a,b,R,dt=1):
     """
     Dynamic normalisation or lightness filling-in
+    ---------------------------------------------------------------------------
+    Using the previously obtained global min values of the input stimuli
+    a lightness diffusion process is used to obtain a normalized image of the 
+    image.
     
-    ISSUE : R (+1) regularisation parameter in steady state solution to buffer NaN
+    ISSUE : R (+1) regularisation parameter in steady state solution required
+            to buffer NaN
     
     Returns
     --------
-    d     - steady state solution
+    c     - steady state solution
     c_out - dynamic solution
     """
     
@@ -167,7 +212,7 @@ def ONtype_norm(s,t0,h,N,D,t_N,a,b,R,dt=0.001):
     cd_out = np.zeros((t_N,N,N))
 
     for t in np.arange(0,t_N-1):
-        c[t,:,:] =  (s-a[t,:,:])/(b[t,:,:]-a[t,:,:]+1)
+        c[t,:,:] =  (s-a[t,:,:])/(b[t,:,:]-a[t,:,:]+R)
     
         cd_out_1 = b[t,:,:]*(np.zeros((N,N))-cd_out[t,:,:])
         cd_out_2 = a[t,:,:]*(np.ones((N,N))-cd_out[t,:,:])
@@ -176,9 +221,11 @@ def ONtype_norm(s,t0,h,N,D,t_N,a,b,R,dt=0.001):
     return c , cd_out
     
 
-def OFFtype_norm(t_N,N,a,b,c,s,R):
+def OFFtype_norm(t_N,N,a,b,c,s,R,dt=1):
     """
     Inverse dynamic normalisation or darkness filling-in
+    --------------------------------------------------------------------------
+    Same as dynamic normalisation, but opposite.
     
     ISSUE : R (+1) regularisation parameter in steady state solution to buffer NaN    
     
@@ -193,73 +240,76 @@ def OFFtype_norm(t_N,N,a,b,c,s,R):
     for t in np.arange(0,t_N-1):
         d[t,:,:] = (b[t,:,:] - s) / (b[t,:,:] - a[t,:,:]+R)      
        
-        d_out_1 = b[t,:,:]*(1-d_out[t,:,:])
+        d_out_1 = b[t,:,:]*(np.ones((N,N))-d_out[t,:,:])
         d_out_2 = a[t,:,:]*(np.zeros((N,N))-d_out[t,:,:])
-        d_out[t,:,:] = d_out_1 - d_out_2 - s
+        d_out[t,:,:] = dt*(d_out_1 - d_out_2 - s)
     return d, d_out
     
 
 """ 
-RUN CODE
-
-Parameters
--------------
-N : int     Image dimension
-D : int     Diffusion coefficient - weights rate of lightness diffusion [0<D<1]
-h : int     Runga-Kutta integration step h>0
-t0 : int    Stimulus injection times
-tN : int    Length of stimulation
+Here is the code to run
 """
+# Parameters
+D = 0.01  # Diffusion Coefficient
+h = 1     # Runga-Kutta Step
+t0 = 0    # Start time
+t_N = 500 # End time = (Length of stimulation)
+R = 1     # Regularisation parameter
 
-
-# Import jpg image or use square wave stimulus, resize, convert into usable array
+# Import jpg image or use square wave stimulus
 im = Image.open("/home/will/gitrepos/contAdaptTranslation/Documents/whites.jpg").convert('L')
+
+# Resizing image (smaller) increases speed (but reduces accuracy)
 arr = np.array(im.resize((100,100), Image.ANTIALIAS))
+
+# Scale down from grey to binary scale
 stimulus=arr/255.
+
+#convert into usable array
 N=arr.shape[0]
 
-# Parameters
-D = 0.01 # Diffusion Coefficient
-h = 1    # Runga-Kutta Step
-t0 = 0   # Start time
-t_N = 500# End time
-R = 1    #regularisation parameter
-
+"""
+To increase code speed, multiple processors are run simultaneously with different
+diffusion states. The results are saved in the "results" array.
+"""
 def multi_run_wrapper(args):
-   return Runge_Kutta(*args)
+   return Solve_diff_eq_RK4(*args)
 
 # Three diffusion behaviour states
-pool = Pool(4)
+pool = Pool(4) # Open 4 processors available on this computer into Pools
 state1=(stimulus,-1,t0,h,N,D,t_N)
 state2=(stimulus,0,t0,h,N,D,t_N)
 state3=(stimulus,1,t0,h,N,D,t_N)
 results = pool.map(multi_run_wrapper,[state1,state2,state3])
 pool.close()
 
-a=results[0]
-b=results[2]
-ss=results[1]
+# output results into arrays
+a=results[0]  # minimum growth state of diffusion
+b=results[2]  # maximum growth state of diffusion
+ss=results[1] # steady-state diffusion
 
-# Two diffusion layers
+# Two diffusion layers converging to normalized image 
 c, c_out = ONtype_norm(stimulus,t0,h,N,D,t_N,a,b,1) # Lightness filling-in
 d, d_out = OFFtype_norm(t_N,N,a,b,c,stimulus,1)     # Darkness filling-in
 
 maxval = np.zeros_like(c)
+
+
 # Steady-state half-wave-rectify
 S_bright = np.array([c, maxval]).max(axis=0)
-S_dark   = np.array([d, maxval]).max(axis=0)
+S_dark   = np.array([d, maxval]).min(axis=0)
 
-# Dynamic half-wave-rectify
-S_bright_d = np.array([c_out, maxval]).max(axis=0)*1000
-S_dark_d   = np.array([d_out, maxval]).max(axis=0)*1000
+# Dynamic state half-wave-rectify
+S_bright_d = np.array([c_out, maxval]).max(axis=0)
+S_dark_d   = np.array([d_out, maxval]).min(axis=0)
 
 # Perceptual activities
 P = (S_bright-S_dark)/(1+S_bright+S_dark) # Steady-state
 P_d = (S_bright_d-S_dark_d)/(1+S_bright_d+S_dark_d) # Dynamic
 
 # Positive values only
-P  = np.array([P, maxval]).max(axis=0)
-P_d= np.array([P_d, maxval]).max(axis=0)
+#P  = np.array([P, maxval]).max(axis=0)
+#P_d= np.array([P_d, maxval]).max(axis=0)
 
 
 
@@ -283,26 +333,26 @@ plot_r=np.arange(1,t_N,50)
 plot_max=0.1
 
 f, axarr = plt.subplots(3, 6)
-axarr[0, 0].imshow(plotter1[plot_r[0],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[0, 1].imshow(plotter1[plot_r[1],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[0, 2].imshow(plotter1[plot_r[2],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[0, 3].imshow(plotter1[plot_r[3],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[0, 4].imshow(plotter1[plot_r[4],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[0, 5].imshow(plotter1[plot_r[5],:,:], cmap='gray',vmax=1,vmin=0)
+axarr[0, 0].imshow(plotter1[plot_r[0],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[0, 1].imshow(plotter1[plot_r[1],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[0, 2].imshow(plotter1[plot_r[2],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[0, 3].imshow(plotter1[plot_r[3],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[0, 4].imshow(plotter1[plot_r[4],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[0, 5].imshow(plotter1[plot_r[5],:,:], cmap='gray')#,vmax=1,vmin=0)
 
-axarr[1, 0].imshow(plotter2[plot_r[0],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[1, 1].imshow(plotter2[plot_r[1],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[1, 2].imshow(plotter2[plot_r[2],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[1, 3].imshow(plotter2[plot_r[3],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[1, 4].imshow(plotter2[plot_r[4],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[1, 5].imshow(plotter2[plot_r[5],:,:], cmap='gray',vmax=1,vmin=0)
+axarr[1, 0].imshow(plotter2[plot_r[0],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[1, 1].imshow(plotter2[plot_r[1],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[1, 2].imshow(plotter2[plot_r[2],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[1, 3].imshow(plotter2[plot_r[3],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[1, 4].imshow(plotter2[plot_r[4],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[1, 5].imshow(plotter2[plot_r[5],:,:], cmap='gray')#,vmax=1,vmin=0)
 
-axarr[2, 0].imshow(plotter3[plot_r[0],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[2, 1].imshow(plotter3[plot_r[1],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[2, 2].imshow(plotter3[plot_r[2],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[2, 3].imshow(plotter3[plot_r[3],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[2, 4].imshow(plotter3[plot_r[4],:,:], cmap='gray',vmax=1,vmin=0)
-axarr[2, 5].imshow(plotter3[plot_r[5],:,:], cmap='gray',vmax=1,vmin=0)
+axarr[2, 0].imshow(plotter3[plot_r[0],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[2, 1].imshow(plotter3[plot_r[1],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[2, 2].imshow(plotter3[plot_r[2],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[2, 3].imshow(plotter3[plot_r[3],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[2, 4].imshow(plotter3[plot_r[4],:,:], cmap='gray')#,vmax=1,vmin=0)
+axarr[2, 5].imshow(plotter3[plot_r[5],:,:], cmap='gray')#,vmax=1,vmin=0)
 
 # Luminance edge profiler
 plt.figure(2,figsize=[4,13])
@@ -371,3 +421,38 @@ plt.title('Output Percept')
 #f_out = Runge_Kutta(stimulus,0,t0,h,N,D,t_N) # Equilibrium
 #a = Runge_Kutta(stimulus,-1,t0,h,N,D,t_N) # Minimum syncytiun (evolves into global minimum)
 #b = Runge_Kutta(stimulus,1,t0,h,N,D,t_N) # Max syncytium (evolves into global maximum)
+
+#
+#im = Image.open("/home/will/gitrepos/contAdaptTranslation/Documents/whites.jpg").convert('L')
+#arr = np.array(im.resize((500,500), Image.ANTIALIAS))
+#stimulus=arr/255.
+#
+#plt.subplot(4,1,1)
+#plt.imshow(stimulus,cmap='jet')
+#plt.subplot(4,1,2)
+#plt.imshow(stimulus,cmap='PiYG')
+#plt.subplot(4,1,3)
+#plt.imshow(stimulus,cmap='bwr')
+#plt.subplot(4,1,4)
+#plt.imshow(stimulus,cmap='Greens')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
